74822051ba5c01de98b21dd0725424392d513d61
Tyler Spring <TylerGSpring@gmail.com>
2021-11-12 20:00:55 -0500

    Add files via upload

--- project_2.java
+++ project_2.java
@@ -6,23 +6,43 @@ import javax.swing.*;
 public class project_2 {
 	
 	//ArrayList made here
-	private static List<Poly>numList = new ArrayList<>();
+	private static List<Poly>numberList = new ArrayList<>();
 	
 	//Main Method and create file chooser object
-	public static void main(String[] args) {
+	public static ArrayList<String> main(String[] args) 
+	{
+		ArrayList<String>polyList = new ArrayList<>();
 		JFileChooser reader = new JFileChooser();
 		reader.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 	reader.setCurrentDirectory(null);
 	int input = reader.showOpenDialog(null);
-	
-	//Populates array here
-	for (int i = 0; i < ;i++ )
+	if (input == JFileChooser.APPROVE_OPTION)
 	{
-		
-	}
-	if(input == JFileChooser.APPROVE_OPTION)
+	File file = reader.getSelectedFile();
+	try
 	{
-			System.out.println("This shit sucks" + reader.getSelectedFile());
+		Scanner scan = new Scanner(file);
+		if(file.isFile())
+		{
+			while (scan.hasNextLine())
+			{
+				String input1 = scan.nextLine();
+				polyList.add(input1);
+			}
+			}
 		}
+		catch (NoSuchElementException nse)
+		{
+			JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "wrong");
+		}
+	catch(FileNotFoundException fne)
+	{
+		JOptionPane.showMessageDialog(JOptionPane.getRootFrame(), "idiot");
 	}
-}
+	}
+	
+
+	return polyList;
+	
+	}
+	}
