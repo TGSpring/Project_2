93400ce67e6df181816365c45e0bf567bb979ba9
Tyler Spring <TylerGSpring@gmail.com>
2021-12-29 14:09:10 -0500

    Update invalidPoly.java

--- invalidPoly.java
+++ invalidPoly.java
@@ -1,5 +1,16 @@
 package project_2;
 
 public class invalidPoly extends RuntimeException {
+	/**
+	 * @author tyler
+	 *Project 2
+	 *11/18/21
+	 *This is the invalidPoly class, this is where the RuntimeException can be called when the array needs it. It just displays a window with an
+	 *error message.
+	 */
+	public invalidPoly(String string) {
+		super(string);
+
+	}
 
 }
